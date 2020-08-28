822a3da6fd9246b03e2efe9906f27ff8765ab9bc
Arpit Vasani <vasani.arpit@gmail.com>
2020-08-28 23:10:25 +0530

    adding post script

---
+++ printMessage.js
@@ -0,0 +1,9 @@
+const logo = `
+
+╔═╗ ╦═╗  ┌─┐┌─┐┌┬┐┌─┐  ┌─┐┌─┐┌─┐┌┐┌┌┐┌┌─┐┬─┐
+║═╬╗╠╦╝  │  │ │ ││├┤   └─┐│  ├─┤││││││├┤ ├┬┘
+╚═╝╚╩╚═  └─┘└─┘─┴┘└─┘  └─┘└─┘┴ ┴┘└┘┘└┘└─┘┴└─
+
+Don't forget to install requirements in your machine. 
+`
+console.log(logo);
